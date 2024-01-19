ab2646b0ff84a54136331dbec0e2f8f90e150b3c
shubhambathla <shubhambathla116@gmail.com>
2024-01-18 19:08:58 -0500

    Defined variables

--- main.go
+++ main.go
@@ -3,5 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Print("Hello Class!")
+	fmt.Print("Hello Class!\n")
+	var text string
+	var digit int
+	text = "shubham"
+	digit = 10
+	fmt.Printf("%s%d\n", text, digit)
 }
